146e33bcf4097a0a4001dc8476f7ef6e556ad994
yanok5 <yana.nenasheva2001@gmail.com>
2020-11-03 23:27:04 +0300

    Сделано

--- include/stackqueue.h
+++ include/stackqueue.h
@@ -1,1 +1,224 @@
-﻿
+﻿#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX_STACK_SIZE = 1000;
+
+template <typename T>
+class Stack
+{
+protected:
+	T *pStack;
+	int Size;
+	int Max;
+public:
+	Stack(int s = 10);
+	Stack(const Stack &s);
+	~Stack();
+	void Push(T elem);	// Добавить элемент в стек
+	void Pop();		// Удалить элемент из стека
+	bool Empty();	// Проверка на пустоту стека
+	bool Full();	// Проверка на полноту стека
+	T Top();
+	Stack& operator=(const Stack &s);
+	bool operator==(const Stack &s) const;
+	bool operator!=(const Stack &s) const;
+	T operator[](int pos);
+};
+
+template <typename T>
+Stack<T>::Stack(int s)
+{
+	if (s < 0 || s > MAX_STACK_SIZE)
+		throw "Smth wrong!";
+	Size = 0;
+	Max = s;
+	pStack = new T[s];
+}
+
+template <typename T>
+Stack<T>::Stack(const Stack &s)
+{
+	Size = s.Size;
+	Max = s.Max;
+	pStack = new T[Max];
+	for (int i = 0; i < Size; i++)
+		pStack[i] = s.pStack[i];
+}
+
+template <typename T>
+Stack<T>::~Stack()
+{
+	if (pStack)
+	{
+		delete[] pStack;
+		pStack = nullptr;
+	}
+}
+
+template <typename T>
+void Stack<T>::Push(T elem)
+{
+	if (Full())
+	{
+		Max = 2.0 * Max;
+		T* temp = new T[Max];
+		for (int i = 0; i < Size; i++)
+			temp[i] = pStack[i];
+		if (pStack)
+			delete[]pStack;
+		pStack = temp;
+	}
+	pStack[Size++] = elem;
+}
+
+template <typename T>
+void Stack<T>::Pop()
+{
+	if (Empty())
+		throw "Stack is empty";
+	else
+	{
+		pStack[--Size] = 0;
+	}
+}
+
+template <typename T>
+bool Stack<T>::Empty()
+{
+	return Size == 0;
+}
+
+template <typename T>
+bool Stack<T>::Full()
+{
+	return Max == Size;
+}
+
+template<typename T>
+T Stack<T>::Top()
+{
+	if (Empty())
+		throw "Stack is empty";
+	return pStack[Size - 1];
+}
+
+template <typename T>
+Stack<T>& Stack<T>::operator=(const Stack &s)
+{
+	Max = s.Max;
+	Size = s.Size;
+	if (pStack)
+		delete[] pStack;
+	pStack = new T[Max];
+
+	for (int i = 0; i < Size; i++)
+		pStack[i] = s.pStack[i];
+	return *this;
+}
+
+template <typename T>
+bool Stack<T>::operator==(const Stack &s) const
+{
+	if ((Size != s.Size) || (Max != s.Max))
+		return false;
+	for (int i = 0; i < Max; i++)
+	{
+		if (pStack[i] != s.pStack[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+template <typename T>
+bool Stack<T>::operator!=(const Stack &s) const
+{
+	return !(s == *this);
+}
+
+template <class T>
+T Stack<T>::operator[](int pos)
+{
+	if (pos < 0 || pos > Max)
+		throw "Smth wrong";
+	return pStack[pos];
+}
+/*----------------------------------------------------------*/
+
+template <class T>
+class Queue : public Stack<T>
+{
+private:
+	Stack<int> s1, s2;
+	int Ise; //Index start element
+public:
+	Queue(int q = 6);
+	~Queue() {};
+	void Push(T elem);
+	void Pop();
+	bool operator==(const Queue &q);
+	bool operator!=(const Queue &q);
+	T operator[](int pos);
+};
+
+template <class T>
+Queue<T>::Queue(int q)
+{
+	Stack<T> temp(q);
+	s1 = temp;
+	s2 = temp;
+	Ise = 0;
+}
+
+template <class T>
+void Queue<T>::Push(T elem)
+{
+	s1.Push(elem);
+}
+
+template <class T>
+void Queue<T>::Pop()
+{
+	while (!s1.Empty())
+	{
+		s2.Push(s1.Top());
+		s1.Pop();
+	}
+	s2.Pop();
+
+	while (!s2.Empty())
+	{
+		s1.Push(s2.Top());
+		s2.Pop();
+	}
+}
+
+template <class T>
+bool Queue<T>::operator==(const Queue &q)
+{
+	if ((!q.s1.Empty()) && (!s1.Empty()))
+		if (s1 == q.s1)
+			return true;
+	if ((!q.s2.Empty()) && (!s2.Empty()))
+		if (s2 == q.s2)
+			return true;
+	return false;
+}
+
+template <class T>
+bool Queue<T>::operator!=(const Queue &q)
+{
+	return !(q == *this);
+}
+
+template <class T>
+T Queue<T>::operator[](int pos)
+{
+	if (s2.Empty())
+		return s1[pos + Ise];
+	return s2[pos];
+}
--- test/test_tstack_tqueue.cpp
+++ test/test_tstack_tqueue.cpp
@@ -1,1 +1,128 @@
+#include "stackqueue.h"
 
+#include <gtest.h>
+
+TEST(Stack, can_create_stack)
+{
+	ASSERT_NO_THROW(Stack<int> s);
+}
+
+TEST(Stack, cant_create_too_large_stack)
+{
+	ASSERT_ANY_THROW(Stack<int> s(MAX_STACK_SIZE + 1));
+}
+
+TEST(Stack, can_create_stack_with_positive_length)
+{
+	ASSERT_NO_THROW(Stack<int> s(10));
+}
+
+TEST(Stack, cant_create_stack_with_negative_length)
+{
+	ASSERT_ANY_THROW(Stack<int> s(-1));
+}
+
+TEST(Stack, cant_create_stack_with_very_big_length)
+{
+	ASSERT_ANY_THROW(Stack<int> s(1001));
+}
+
+TEST(Stack, can_push)
+{
+	Stack<int> s(2);
+	ASSERT_NO_THROW(s.Push(1));
+}
+
+TEST(Stack, can_pop)
+{
+	Stack<int> s;
+	ASSERT_ANY_THROW(s.Pop());
+}
+
+TEST(Stack, can_repack_when_push)
+{
+	Stack<int> s(1);
+	s.Push(1);
+	ASSERT_NO_THROW(s.Push(2));
+	ASSERT_EQ(2, s.Top());
+}
+
+TEST(Stack, throws_when_free)
+{
+	Stack<int> s(2);
+	ASSERT_ANY_THROW(s.Pop());
+}
+
+TEST(Stack, can_get_top)
+{
+	Stack<int> s(3);
+	s.Push(1);
+	ASSERT_NO_THROW(s.Top());
+}
+/*---------------------------------------------------------------------------------------------------------*/
+
+TEST(Queue, can_create_queue)
+{
+	ASSERT_NO_THROW(Queue<int> s);
+}
+
+TEST(Queue, can_push)
+{
+	Queue<int> q;
+	ASSERT_NO_THROW(q.Push(1));
+}
+
+TEST(Queue, no_throw_when_push_into_full_queue)
+{
+	Queue<int> q(3);
+	q.Push(1);
+	q.Push(2);
+	q.Push(3);
+	ASSERT_NO_THROW(q.Push(4));
+}
+
+TEST(Queue, can_pop)
+{
+	Queue<int> q(5);
+	q.Push(1);
+	ASSERT_NO_THROW(q.Pop());
+}
+
+TEST(TQueue, throws_when_pop_from_empty_stack)
+{
+	Queue<int> q;
+	ASSERT_ANY_THROW(q.Pop());
+}
+
+TEST(Queue, no_throw_when_many_pushes_and_pops)
+{
+	Queue<int> q(5);
+	q.Push(1);
+	q.Push(2);
+	q.Push(3);
+	q.Push(4);
+	q.Push(5);
+	q.Pop();
+	q.Push(6);
+	ASSERT_NO_THROW(q.Pop());
+}
+
+TEST(Queue, works_correctly)
+{
+	Queue<int> q(5);
+	q.Push(1);
+	q.Push(2);
+	q.Push(3);
+	q.Push(4);
+	q.Push(5);
+	q.Pop();
+	q.Push(6);
+	EXPECT_EQ(6, q[4]);
+}
+
+TEST(Queue, can_return_element)
+{
+	Queue<int> q(5);
+	q.Push(1);
+	ASSERT_NO_THROW(q[0]);
+}
